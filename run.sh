50f4be0a8def7012d26e9cb2717c9a4250bb3aba
MickeyJ <develop.agility@gmail.com>
2018-12-03 07:31:59 -0700

    more clean up

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 COUNT_FILE="${CWD}/count.txt"
 INDEX=$(cat ${COUNT_FILE})
 PEOPLE=('abby' 'miki' 'sabir' 'marah')
+PEOPLE_COUNT=${#PEOPLE[@]}
 
 echo "end ${PEOPLE[INDEX]}s turn"
 say -v Carlos "${PEOPLE[INDEX]} [[slnc 500]] taim tu passit"
 
-if [[ ${INDEX} -eq $(( ${#PEOPLE[@]} - 1)) ]]; then
+if [[ ${INDEX} -eq $(( ${PEOPLE_COUNT} - 1)) ]]; then
 	INDEX=0
 else
 	INDEX=$(( $INDEX + 1 ))
